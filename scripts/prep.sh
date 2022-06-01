40f29bddd36f2e4972e4a9bfcf9eef2eda82e121
MrNerdHair <mrnerdhair@gmail.com>
2022-05-31 20:19:37 -0400

    chore: update web and rebase patch

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -8,6 +8,7 @@ git submodule update --init --recursive
 pushd web
 git reset --hard
 git clean -fdxe node_modules
+git config --local core.excludesFile ../web.exclude
 git -c "user.name=build.sh" -c "user.email=build.sh" am ../web.patch
 ln -s ../build build
 popd
