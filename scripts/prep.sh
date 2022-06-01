49d47a17cddd54170e18a61ec2ff39e00e9fa42d
MrNerdHair <mrnerdhair@gmail.com>
2022-05-31 21:09:08 -0400

    chore: deterministic output for `yarn rebase-patch`

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -6,6 +6,8 @@ mkdir ./build
 git reset web
 git submodule update --init --recursive
 pushd web
+git am --abort 2>/dev/null || true
+git rebase --abort 2>/dev/null || true
 git reset --hard
 git clean -fdxe node_modules
 git config --local core.excludesFile ../web.exclude
--- scripts/update-patch.sh
+++ scripts/update-patch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 rm -f 1
 pushd web
+if ! git log --format=%B -n 1 | head -n 1 | grep -Eq '^ALPHA PATCH, DO NOT MERGE$'; then
+  echo "error: HEAD of submodule must be titled 'ALPHA PATCH, DO NOT MERGE'" 1>&2
+  exit 1
+fi
 git format-patch --numbered-files -o .. HEAD~1
-mv ../1 ../web.patch
-git checkout HEAD~1
+git checkout -q HEAD~1
 popd
+tail -n +2 ./1 > ./web.patch
+rm ./1
 git add web web.patch
